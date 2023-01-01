25ddba8d13f8b1f1ffdeecad5701ff1c8e4a51fd
Dan Zaharia <zaharia.danny@gmail.com>
2023-01-01 15:30:32 -0500

    refine color pallette

--- colors.py
+++ colors.py
@@ -1,14 +1,14 @@
 
 colors = {
-    "red123":  0xEE0A0A, # rgb(238, 10, 10);
-    "green456": 0x009328, # rgb(0, 147, 40);
+    "red123":  0xdf0000, # rgb(238, 10, 10);
+    "green456": 0x008700, # rgb(0, 147, 40);
     "magenta7": 0xB933AD, # rgb(185, 51, 173);
-    "blueACE": 0x2850AD, # rgb(40, 80, 173);
-    "orangeBDFM": 0xFF4B00, # rgb(255, 75, 0);
-    "greenG": 0x6EFF32, # rgb(110, 255, 50);
+    "blueACE": 0x0a5786, # rgb(40, 80, 173);
+    "orangeBDFM": 0xff590d, # rgb(255, 75, 0);
+    "greenG": 0x87a375, # rgb(110, 255, 50);
     "brownJZ": 0x996633, # rgb(153, 102, 51);
     "grayL": 0xA7A9AC, # rgb(167, 169, 172);
-    "yellowNQRW": 0xFCCC0A, # rgb(252, 204, 10);
+    "yellowNQRW": 0xfdbc17, # rgb(252, 204, 10);
     "grayS": 0x808183, # rgb(128, 129, 131);
     "black": 0x000000, # rgb(0, 0, 0);
     "white": 0xffffff,# rgb(255, 255, 255);
--- secrets.py
+++ secrets.py
@@ -3,5 +3,5 @@ secrets = {
     'password': 'thanksdan',
     'sign_id': 'kshf',
     'aio_username': 'dzaharia', 
-    'aio_key': 'aio_ytvA41ciRBcdSfoAHVEnkWzXo94s'
+    'aio_key': 'aio_ovVW20QEsXuWTFM6oTp4zZrjWXyt'
 }
